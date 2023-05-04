6134b94e5d3fbb6b787f7ad6617081705f8d2129
Zarina <sibayevaz@mail.ru>
2023-05-04 11:25:40 +0300

    Add Project

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using DancingSchool.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DancingSchool
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static DancingEntities db = new DancingEntities();
+    }
+}
